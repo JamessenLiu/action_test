15fce6ad0c7a4a601b1584366dd7ba65ca90f83f
liumaosen <461614816@qq.com>
2021-05-26 16:29:49 +0800

    add redis action

--- config/test_conf.py
+++ config/test_conf.py
@@ -17,21 +17,21 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://:password@144.34.188.213:65431/0",
+        "LOCATION": "redis://:password@127.0.0.1:65431/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     },
     "ip": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://:password@144.34.188.213:65431/1",
+        "LOCATION": "redis://:password@127.0.0.1:65431/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     },
     "session": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://:password@144.34.188.213:65431/2",
+        "LOCATION": "redis://:password@127.0.0.1:65431/2",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
